51cf6ad0dc95c34d18de74e4d94de975d3907f15
PhamPhien-gif <toilaphien2302@gmail.com>
2025-09-20 20:55:59 +0700

    OLAP_LOAN_APPLICATION

---
+++ OLAP/Select.sql
@@ -0,0 +1,14 @@
+
+select * from DIM_EMPLOYMENT;
+select * from DIM_PROPERTY;
+select * from DIM_ADDRESS;
+select * from DIM_INCOME_BAND;
+select * from DIM_CIBIL_BAND;
+select * from DIM_CUSTOMER;
+select * from DIM_GENDER;
+select * from DIM_PURPOSE;
+select * from DIM_LOAN_TYPE;
+select * from DIM_LOAN_STATUS;
+select * from FACT_LOAN_APPLICATION;
+
+
